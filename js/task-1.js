e3f28665517a709ecc27783b039ef796c1e6930f
damian-sirenko <damian.sirenko@gmail.com>
2024-03-16 14:16:18 +0100

    hw-4 done

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,32 @@
-function slugify(title) {
-	const stringNormalized = title.toLowerCase();
-	const array = stringNormalized.split(" ");
-	const slug = array.join('-');
+function isEnoughCapacity(products, containerSize) {
+	const values = Object.values(products);
+	let totalValuesNumber = 0; 
+	for (const value of values) {
+		totalValuesNumber += value;
+	}
+	if (totalValuesNumber <= containerSize) {
+		return true
+	}
+			else {
+				return false;
+			}
+	}
+	
 
-	return slug;
-}
 
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
